c33186fa1f12d7fcc5ed84c6ba9b5f647acc6a12
Francois369 <franc.vdwalt@gmail.com>
2022-12-31 10:34:50 +0200

    Completed Education Section
    Completed Education Section

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -1,36 +1,41 @@
 import { Fragment } from "react";
 import classes from "../custom/custom.module.css";
+import styles from "./Education.module.css";
 import Badge from "../components/ui/Badge/Badge";
 import Card from "../components/ui/Card/Card.js";
+import { FaGraduationCap } from "react-icons/fa";
 function Education() {
   return (
     <Fragment>
       <Badge>Education</Badge>
       <Card>
-        <h6>
-          <b>Bachelors of commerce Logistics Management</b> <br /> 2007 - 2009
+        <h6 className={classes["my-headings"]}>
+          <FaGraduationCap className={styles["education-icon"]} />
+          <b>Bachelors of commerce Honours Logistics Management </b>
+          <br /> 2010
           <br />
           University of Johannesburg
         </h6>
-        <p>
-          <b>Major Subjects:</b> Logistics Management and Marketing Management.
+        <p className={classes["my-paragraph"]}>
+          <b>Major Subjects: </b>
+          <i>Supply Chain Management and Strategic Logistics Management.</i>
           <br />
-          <b>Other Subjects:</b> Transport Economics, Accounting, Business
-          Management
+          <b>Other Subjects:</b> <i>Quantitative Techniques</i>
         </p>
-      </Card>
-
-      <Card>
-        <h6>
-          <b>Bachelors of commerce Logistics Management</b> <br /> 2007 - 2009
+        <br />
+        <h6 className={classes["my-paragraph"]}>
+          <FaGraduationCap className={styles["education-icon"]} />
+          <b>Bachelors of commerce Logistics Management</b>
+          <br /> 2007 - 2009
           <br />
           University of Johannesburg
         </h6>
-        <p>
-          <b>Major Subjects:</b> Logistics Management and Marketing Management.
+        <p className={classes["my-paragraph"]}>
+          <b>Major Subjects:</b>{" "}
+          <i>Logistics Management and Marketing Management.</i>
           <br />
-          <b>Other Subjects:</b> Transport Economics, Accounting, Business
-          Management
+          <b>Other Subjects:</b>{" "}
+          <i>Transport Economics, Accounting, Business Management</i>
         </p>
       </Card>
     </Fragment>
