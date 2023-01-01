392ae26b1231b01a09a13aff08becbaa6af0cde6
Francois369 <franc.vdwalt@gmail.com>
2023-01-01 11:45:55 +0200

    Started Context Store
    Started ContextStore

---
+++ src/store/ContextStore.js
@@ -0,0 +1,6 @@
+import { useState, createContext } from "react";
+import ReactDOM from "react-dom/client";
+
+
+https://www.w3schools.com/react/react_usecontext.asp
+const UserContext = createContext();
