3c074cbd0d8fd450cb99c82e262a87d3703aca51
Francois369 <franc.vdwalt@gmail.com>
2023-01-03 20:55:41 +0200

    finished references page
    finished references page

--- src/pages/Refrences.js
+++ src/pages/Refrences.js
@@ -1,6 +1,9 @@
 import { Fragment } from "react";
 import Card from "../components/ui/Card/Card";
 import Badge from "../components/ui/Badge/Badge";
+import classes from "../custom/custom.module.css";
+import styles from "./references.module.css";
+import { FaReact } from "react-icons/fa";
 
 const References = () => {
   const refList = [
@@ -16,12 +19,32 @@ const References = () => {
       Position: "Head of Department Financial Management",
       tel: "+27 73 660 0961",
     },
+    {
+      Name: "Andrew Brooks",
+      Company: "XFour Solutions",
+      Position: "Project Manager ",
+      tel: "+27 83 455 6280",
+    },
+    {
+      Name: "Jaco Smit",
+      Company: "XFour Solutions",
+      Position: "Director ",
+      tel: "+27 84 585 5509",
+    },
+    {
+      Name: "Shaun O'Reilly",
+      Company: "XFour Solutions",
+      Position: "Director ",
+      tel: "+27 82 56 54 284",
+    },
+    {
+      Name: "Johan van Aswegen",
+      Company: "Imperial Logistics",
+      Position: "Operations Executive ",
+      tel: "+27 83 449 6590",
+    },
   ];
-  // •	Bellina Sebesho> FSCA> Head of Department Financial Management > +27 73 660 0961
-  // •	Andrew Brooks> XFour - Project Manager  > +27 83 455 6280
-  // •	Jaco Smit > XFour > Director > +27 84 585 5509
-  // •	Shaun O Reilly > XFour > Director > +27 82 56 54 284
-  // •	Johan van Aswegen > Imperial Logistics – Operations Executive > +27 83 449 6590
+
   // •	Dr Peter Kilbourn > University of Johannesburg - Senior Lecturer  >  See attached reference letter
 
   const renderList = refList.map(buildRefrenceitem);
@@ -29,10 +52,21 @@ const References = () => {
   function buildRefrenceitem(item) {
     return (
       <div key={Math.random()}>
-        <p>
-          {item.Name}
+        <p className={classes["my-paragraph"]}>
+          <strong>
+            <i>
+              <FaReact className={styles["reference-icon"]} />
+              {" " + item.Name}
+            </i>
+          </strong>
           <br />
+          <strong>&emsp; {item.Company}</strong>
+          <br />
+          <i>&emsp;{item.Position}</i>
+          <br />
+          &emsp;{item.tel}
         </p>
+        <br />
       </div>
     );
   }
